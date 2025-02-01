36a676047d44cda72fcbd2a2783c94e205e5a5fb
Julian Dalban <julian.dalban@gmail.com>
2025-01-31 21:35:55 -0500

    Working on structure

--- to_do_API.py
+++ to_do_API.py
@@ -1,2 +1,49 @@
 from fastapi import FastAPI, HTTPException
+from pydantic  import BaseModel, Field
 
+app = FastAPI(title='To Do List API')
+
+# Defining what the input values should look like.
+class Task(BaseModel):
+    title: str = Field(
+        ...,
+        min_length=1,
+        max_length=50,
+        description='The title of the given task.'
+    ),
+    description: str = Field(
+        default=None,
+        min_length=1,
+        max_length=1000,
+        description='Describes the task, what is required.'
+    ),
+    status: str = Field(
+        ...,
+        pattern="^(pending|in-progress|completed)$",
+        description='The status of the task.'
+    ),
+    priority: int = Field(
+        ...,
+        ge=1,
+        le=5,
+        description='Describes the priority of the task.'
+    )
+    class Config:
+        schema_extra = {
+            'example' : {
+                'title': 'Pushups for the week',
+                'description': '''Attempting to do 50 pushups a day for the next week.
+                I would like to atleast have done a minimum of 30 per day.''',
+                'status': 'pending',
+                'priority': 1
+            }
+        }
+
+
+# Defining the mechanism of storing the task
+class TaskStore: 
+    def __init__(self):
+        self.tasks = dict[Task] = dict()
+    
+    def add_task(Task):
+        if tasks.
